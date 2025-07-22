88e4db1e311045e92405cc9d13eab2541db9d608
Silyu Li <silyu.li@rwth-aachen.de>
2025-07-22 21:56:03 +0200

    add log for receiver

--- receiver.py
+++ receiver.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/receive', methods=['POST'])
 def receive_data():
     data = request.get_json()
-    print("Received Data:", data)
+    res_time = datetime.now()
+    print("Received Data at",res_time.isoformat())
     return {'status': 'success'}, 200
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000)
