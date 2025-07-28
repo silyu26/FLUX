5eeef9a0dc0e23bfcc43c1c1ff6ee79d7be31a50
Silyu Li <silyu.li@rwth-aachen.de>
2025-07-29 00:11:12 +0200

    multiple receivers

--- receiver_http.py
+++ receiver_http.py
@@ -4,10 +4,12 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/receive', methods=['POST'])
+
 def receive_data():
     data = request.get_json()
     res_time = datetime.now()
     print("Received Data at",res_time.isoformat())
     return {'status': 'success'}, 200
+
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000)
