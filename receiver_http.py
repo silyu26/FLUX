af56d8f0a2ccf72ea211b2e8ec55a01cfb3b577f
Silyu Li <silyu.li@rwth-aachen.de>
2025-07-31 16:58:54 +0200

    added n8n

--- receiver_http.py
+++ receiver_http.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -9,7 +10,16 @@ def receive_data():
     data = request.get_json()
     res_time = datetime.now()
     print("Received Data at",res_time.isoformat())
+    forward_to_n8n(data)
     return {'status': 'success'}, 200
+
+def forward_to_n8n(data):
+    try:
+        data["received_at"] = datetime.now().isoformat()
+        #requests.post("http://localhost:5678/webhook-test/http_data", json=data)
+        requests.post("http://localhost:5678/webhook/http_data", json=data)
+    except Exception as e:
+        print(f"Error sending to n8n: {e}")
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000)
