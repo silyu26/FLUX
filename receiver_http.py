860fcda28926e10daf39cd0ae22179ad798cc7e2
Silyu Li <silyu.li@rwth-aachen.de>
2025-08-20 00:14:54 +0200

    major changes

--- receiver_http.py
+++ receiver_http.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 from datetime import datetime
 import requests
+#from . import crud, model, db
+from model import Experiment, WeatherData
+from crud import create_experiment_with_weather
+from db import SessionLocal
+
 
 app = Flask(__name__)
 
@@ -9,8 +14,26 @@ app = Flask(__name__)
 def receive_data():
     data = request.get_json()
     res_time = datetime.now()
-    print("Received Data at",res_time.isoformat())
-    forward_to_n8n(data)
+    #print("Received Data at",res_time.isoformat())
+    #forward_to_n8n(data)
+    exp = Experiment(
+        gen_at=data['gen_at'],
+        exp_id=1,
+        model_in=datetime.now(),
+        model_out=datetime.now(),
+        server_in=res_time,
+        #model_out=datetime.now()
+    )
+    weather = WeatherData(
+        date=data['date'],
+        precipitation=data['precipitation'],
+        temp_max=data['temp_max'],
+        temp_min=data['temp_min'],
+        wind=data['wind'],
+    )
+    exp.weather = weather
+
+    create_experiment_with_weather(SessionLocal(), exp)
     return {'status': 'success'}, 200
 
 def forward_to_n8n(data):
