7caadf79f6a8ec73f942b3a4e64420141e38a4cc
Silyu Li <silyu.li@rwth-aachen.de>
2025-09-25 11:12:11 +0200

    Added all models

--- YOLO/yolo_v11_csfy.py
+++ YOLO/yolo_v11_csfy.py
@@ -13,7 +13,7 @@ from model import Experiment, WeatherData
 
 app = FastAPI(title="YOLOv11 API", description="API for YOLOv11 inference", version="1.0")
 #uvicorn YOLO.yolo_v11_csfy:app --reload --host 0.0.0.0 --port 5000
-model = YOLO("yolo11n.pt")
+model = YOLO("yolo11m.pt")
 expId = 9
 
 def limit_resources():
